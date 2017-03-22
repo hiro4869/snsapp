efbeeffe056805a2ce692cdbe06f12d77e800d50
hiro4869 <sakuragima48@gmail.com>
2017-03-22 17:29:28 +0900

    first commit

---
+++ snsapp/app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  before_action:set_post, only:[:show, :edit, :update, :destroy]
+
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render new_post_path
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+
+
+  private
+
+  def post_params
+    params[:post].permit(:title, :body)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+end
